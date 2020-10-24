6680b07e3dc0fcbbe351c762eb87808f7f1bd642
alireza <alirezafazeli34@gmail.com>
2020-10-24 09:29:49 +0330

    i made Category model adn admin model for Category

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from Blog.models import Article, Category
 
 # Register your models here.
+class Category_Admin(admin.ModelAdmin):
+    list_display = ('Position', 'Status', 'Published')
+    list_filter = (['Status'])
+    search_fields = ('Title', 'Slug')
+    prepopulated_fields = {'Slug':('Title',)}
+
+admin.site.register(Category, Category_Admin)
+
+class Article_Admin(admin.ModelAdmin):
+    list_display = ('Title', 'Status', 'Published')
+    list_filter = ('Published', 'Status')
+    search_fields = ('Title', 'Slug', 'Descraption')
+    prepopulated_fields = {'Slug':('Title',)}
+
+admin.site.register(Article, Article_Admin)
--- Blog/models.py
+++ Blog/models.py
@@ -2,6 +2,23 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    Title = models.CharField(max_length=50)
+    Slug = models.CharField(max_length=50, unique=True)
+    Position = models.IntegerField(unique=True)
+    Published = models.DateField(default=timezone.now)
+    Status = models.BooleanField(default=True, verbose_name='To Be Published ?')
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Category'
+        ordering = [
+            'Position',
+        ]
+    def __str__(self):
+        return self.Title    
+
+
 
 class Article(models.Model):
     Status_Choices = {
@@ -13,8 +30,16 @@ class Article(models.Model):
     Image = models.ImageField(upload_to='photo')
     descraption = models.TextField()
     Writer_Name = models.CharField(max_length=50, verbose_name='writer name :')
+    Category = models.ManyToManyField(Category)
     Published = models.DateField(default=timezone.now)
     Status = models.CharField(max_length=1, choices=Status_Choices, default='P')
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Article'
+
+    def __str__(self):
+        return self.Title   
 
 
 
