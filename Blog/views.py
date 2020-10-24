cd54365ef593165c29f43ebb5de14eab03a16b11
alireza <alirezafazeli34@gmail.com>
2020-10-24 15:31:06 +0330

    kamel kardan view home va kamle kardan blog.html

--- Blog/views.py
+++ Blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from Blog.models import Article, Category
 
+
 # Create your views here.
+
 def Home(request):
     context = {
-        'Article' : Article.objects.filter(Status='P')
+        'Article' : Article.objects.filter(Status='P').order_by('-Published')[:8],
     }
     return render(request, 'blog/blog.html', context)
