41069803ee8a7c394891776b74b3a6916c9d73c5
alireza <alirezafazeli34@gmail.com>
2020-10-26 00:56:07 +0330

    category takmil shod va safhasho sakhtam ke ta daste be fuck raftam :))))

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from Blog.views import Home
+from Blog.views import Home, category_of_site
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', Home, name='home_url')
+    path('', Home, name='home_url'),
+    path('category/<str:Slug>', category_of_site, name='Category_url' )
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from Blog.models import Article, Category
 from django.core.paginator import Paginator
 
@@ -12,7 +12,23 @@ def Home(request):
     context = {
         'Article' : Article.objects.filter(Status='P').order_by('-Published')[:8],
         'Article_2' : Article.objects.filter(Status='P').order_by('-Published')[:4],
-        'Category' : Category.objects.filter(Status= True),
+        'Category' : Category.objects.filter(Status=True),
         'page_obj' : page_object,
     }
     return render(request, 'blog/blog.html', context)
+
+
+def category_of_site(request, Slug):
+    article_model = Article.objects.all()
+    paginator = Paginator(article_model, 8)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+    category_obj = Category.objects.filter(Slug=Slug, Status=True).first()
+    show_category = Category.objects.filter(Status=True)
+    context = {
+        'Category' : category_obj,
+        'page_obj' : page_object, 
+        'show_category' : show_category
+    }
+    return render(request, 'blog/category.html', context)
+        
